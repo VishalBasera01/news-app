4708f8eed1b5dd60a7ef341f1f94a6d29440bb3d
VishalBasera <vishalbasera2000@gmail.com>
2024-10-06 18:05:34 +0530

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,102 +4,104 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="bg-blue-600 p-4 shadow-md">
+    <div>
       <h1 className="text-5xl font-bold text-white text-center mb-4">
         News App
       </h1>
-      <nav className="flex justify-center space-x-4">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-              : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-          }
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/india"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-              : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-          }
-        >
-          India
-        </NavLink>
-        <NavLink
-          to="/australia"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-              : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-          }
-        >
-          Australia
-        </NavLink>
-        <NavLink
-          to="/usa"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-              : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-          }
-        >
-          USA
-        </NavLink>
-        <NavLink
-          to="/france"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-              : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-          }
-        >
-          France
-        </NavLink>
-        <NavLink
-          to="/israel"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-              : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-          }
-        >
-          Israel
-        </NavLink>
-        <NavLink
-          to="/russia"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-              : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-          }
-        >
-          Russia
-        </NavLink>
-        <NavLink
-          to="/japan"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-              : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-          }
-        >
-          Japan
-        </NavLink>
-        <NavLink
-          to="/ukraine"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-              : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
-          }
-        >
-          Ukraine
-        </NavLink>
-      </nav>
+      <div className="bg-blue-600 p-4 shadow-md">
+        <nav className="flex justify-center space-x-4">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+                : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/india"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+                : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+            }
+          >
+            India
+          </NavLink>
+          <NavLink
+            to="/australia"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+                : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+            }
+          >
+            Australia
+          </NavLink>
+          <NavLink
+            to="/usa"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+                : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+            }
+          >
+            USA
+          </NavLink>
+          <NavLink
+            to="/france"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+                : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+            }
+          >
+            France
+          </NavLink>
+          <NavLink
+            to="/israel"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+                : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+            }
+          >
+            Israel
+          </NavLink>
+          <NavLink
+            to="/russia"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+                : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+            }
+          >
+            Russia
+          </NavLink>
+          <NavLink
+            to="/japan"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+                : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+            }
+          >
+            Japan
+          </NavLink>
+          <NavLink
+            to="/ukraine"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-red-600 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+                : "bg-blue-700 hover:bg-blue-800 transition duration-300 text-white font-semibold py-2 px-4 rounded"
+            }
+          >
+            Ukraine
+          </NavLink>
+        </nav>
+      </div>
     </div>
   );
 };
